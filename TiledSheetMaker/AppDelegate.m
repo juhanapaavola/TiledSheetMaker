e59f2c248d69600c82b8eca65095dcf2f6e034bb
juhanapaavola <juhana.paavola@gmail.com>
2014-07-08 18:22:03 +0300

    Initial Commit

---
+++ TiledSheetMaker/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TiledSheetMaker
+//
+//  Created by juhana on 08/07/14.
+//  Copyright (c) 2014 juhanapaavola. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
